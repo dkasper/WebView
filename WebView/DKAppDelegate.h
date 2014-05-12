c934586c2e9412b2e342b72bf512a2af43be8e7a
Dave Kasper <dkasper@gmail.com>
2014-05-11 17:36:52 -0700

    Initial Commit

---
+++ WebView/DKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DKAppDelegate.h
+//  WebView
+//
+//  Created by David Kasper on 5/11/14.
+//  Copyright (c) 2014 David Kasper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebView
+//
+//  Created by David Kasper on 5/11/14.
+//  Copyright (c) 2014 David Kasper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DKAppDelegate class]));
+    }
+}
